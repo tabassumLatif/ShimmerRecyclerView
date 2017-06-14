829a999ee2a18c276f52030b753a2733b3252c5a
Tabassun Latif <tabassum.latif@fourspan.com>
2017-06-14 09:52:50 +0500

    library example added

---
+++ app/src/main/java/com/tabassum/shimmerexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tabassum.shimmerexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.assignment.tabassum.shimmerexample.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
